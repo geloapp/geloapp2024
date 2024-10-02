50cde51fc9400bc7a4f9fc8d204ba7ecc544ca4d
geloapp <gelo.app24@gmail.com>
2024-10-02 17:41:23 +0200

    Update test_streamlit.py

--- test_streamlit.py
+++ test_streamlit.py
@@ -230,7 +230,7 @@ def page1():
 def page2():
     # Ajouter le logo de Fifiloc
     # Importer les données Airbnb
-    logo1 = 'logo_final6_fifiloc.png'
+    logo1 = 'logo_final7_fifiloc.png'
 
     st.image(logo1, width=100)  # Ajustez le chemin et la taille selon vos besoins
     st.markdown("<h1 style='font-size:24px;'>Revenus imposables (micro-bic ou réel)</h1>", unsafe_allow_html=True)
