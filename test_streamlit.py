9fd1cb26a5b17ddf475a92df6d979a4c71cafdaa
geloapp <gelo.app24@gmail.com>
2024-10-06 16:42:04 +0200

    Update test_streamlit.py

--- test_streamlit.py
+++ test_streamlit.py
@@ -557,21 +557,21 @@ def page1():
             kpi_col1, kpi_col2, kpi_col3 = st.columns(3)
             with kpi_col1:
                 st.markdown(
-                    f"<div style='border-radius: 50%; background-color: #006400; padding: 13px; text-align: center;'>"
+                    f"<div style='border-radius: 50%; background-color: #006400; padding: 10px; text-align: center;'>"
                     f"<i class='fas fa-sack-dollar' style='font-size: 30px; color: white;'></i><br>"
                     f"<p style='font-size:15px; color:white; font-weight:bold;'>Revenus totaux: {total_revenus:.2f} € sur {total_mois} mois</p>"
                     f"</div>", unsafe_allow_html=True
                 )
             with kpi_col2:
                 st.markdown(
-                    f"<div style='border-radius: 50%; background-color: #FF0000; padding: 13px; text-align: center;'>"
+                    f"<div style='border-radius: 50%; background-color: #FF0000; padding: 10px; text-align: center;'>"
                     f"<i class='fas fa-file-alt' style='font-size: 30px; color: white;'></i><br>"
                     f"<p style='font-size:15px; color:white; font-weight:bold;'>Charges totales: {total_charges:.2f} € sur {total_mois} mois</p>"
                     f"</div>", unsafe_allow_html=True
                 )
             with kpi_col3:
                 st.markdown(
-                    f"<div style='border-radius: 50%; background-color: #db6635; padding: 13px; text-align: center;'>"
+                    f"<div style='border-radius: 50%; background-color: #db6635; padding: 10px; text-align: center;'>"
                     f"<i class='fas fa-sack-dollar' style='font-size: 30px; color: white;'></i><br>"
                     f"<p style='font-size:15px; color:white; font-weight:bold;'>Solde total: {total_solde:.2f} € sur {total_mois} mois</p>"
                     f"</div>", unsafe_allow_html=True
