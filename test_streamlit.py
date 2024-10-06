f5ff17e0e2c1fbf129abae1a66eb575bd3101c20
geloapp <gelo.app24@gmail.com>
2024-10-06 16:37:31 +0200

    Update test_streamlit.py

--- test_streamlit.py
+++ test_streamlit.py
@@ -557,7 +557,7 @@ def page1():
             kpi_col1, kpi_col2, kpi_col3 = st.columns(3)
             with kpi_col1:
                 st.markdown(
-                    f"<div style='border-radius: 10px; background-color: #006400; padding: 20px; text-align: center;'>"
+                    f"<div style='border-radius: 50%; background-color: #006400; padding: 30px; text-align: center;'>"
                     f"<i class='fas fa-sack-dollar' style='font-size: 30px; color: white;'></i><br>"
                     f"<p style='font-size:15px; color:white; font-weight:bold;'>Revenus totaux: {total_revenus:.2f} € sur {total_mois} mois</p>"
                     f"</div>", unsafe_allow_html=True
