8b0eea5f65786d3e2f2281b9da9bd090947d677b
geloapp <gelo.app24@gmail.com>
2024-10-06 15:58:44 +0200

    Update test_streamlit.py

--- test_streamlit.py
+++ test_streamlit.py
@@ -557,21 +557,21 @@ def page1():
             kpi_col1, kpi_col2, kpi_col3 = st.columns(3)
             with kpi_col1:
                 st.markdown(
-                    f"<div style='border-radius: 50%; background-color: #be7a52; padding: 10px; text-align: center;'>"
+                    f"<div style='border-radius: 50%; background-color: #006400; padding: 10px; text-align: center;'>"
                     #f"<div style='border-radius: 50%; background-color: #471100; padding: 10px; text-align: center;'>"
-                    f"<p style='font-size:15px; color:#006400; font-weight:bold;'>Revenus totaux: {total_revenus:.2f} €</p>"
+                    f"<p style='font-size:15px; color:white; font-weight:bold;'>Revenus totaux: {total_revenus:.2f} €</p>"
                     f"</div>", unsafe_allow_html=True
                 )
             with kpi_col2:
                 st.markdown(
-                    f"<div style='border-radius: 50%; background-color: #fdf8f5; padding: 10px; text-align: center;'>"
-                    f"<p style='font-size:15px; color:#FF0000; font-weight:bold;'>Charges totales: {total_charges:.2f} €</p>"
+                    f"<div style='border-radius: 50%; background-color: #FF0000; padding: 10px; text-align: center;'>"
+                    f"<p style='font-size:15px; color:white; font-weight:bold;'>Charges totales: {total_charges:.2f} €</p>"
                     f"</div>", unsafe_allow_html=True
                 )
             with kpi_col3:
                 st.markdown(
-                    f"<div style='border-radius: 50%; background-color: #fdf8f5; padding: 10px; text-align: center;'>"
-                    f"<p style='font-size:15px; color:#FF8C00; font-weight:bold;'>Solde total: {total_solde:.2f} €</p>"
+                    f"<div style='border-radius: 50%; background-color: #db6635; padding: 10px; text-align: center;'>"
+                    f"<p style='font-size:15px; color:white; font-weight:bold;'>Solde total: {total_solde:.2f} €</p>"
                     f"</div>", unsafe_allow_html=True
                 )
 
